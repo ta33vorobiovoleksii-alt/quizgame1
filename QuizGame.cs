cc4888477c0aa692f225f6f71cc21406eb57ad8d
ta33vorobiovoleksii-alt <ta33.vorobiov.oleksii@gmail.com>
2025-11-10 20:59:53 +0200

    Add files via upload

---
+++ QuizGame.cs
@@ -0,0 +1,35 @@
+﻿namespace Quiz
+{
+
+        public class QuizGame
+    {
+        
+            public int Score { get; private set; }
+            public int CorrectAnswers { get; private set; }
+            public int TotalQuestionsAnswered { get; private set; }
+
+            // Відсоток правильних відповідей (0 якщо ще не було запитань)
+            public double CorrectAnswerPercentage =>
+                TotalQuestionsAnswered == 0
+                    ? 0.0
+                    : (double)CorrectAnswers / TotalQuestionsAnswered * 100.0;
+
+            public void GiveAnswer(bool isCorrect, int points)
+            {
+                TotalQuestionsAnswered++;
+
+                if (isCorrect)
+                {
+                    CorrectAnswers++;
+                    Score += points;
+                }
+            }
+
+            public void Restart()
+            {
+                Score = 0;
+                CorrectAnswers = 0;
+                TotalQuestionsAnswered = 0;
+            }
+        }    
+}
---
+++ QuizGameTests.cs
@@ -0,0 +1,97 @@
+﻿using Xunit;
+using Quiz; // Підключаємо основний проєкт
+
+namespace Quiz.Tests
+{
+    public class QuizGameTests
+    {
+        [Fact]
+        public void Constructor_WhenGameStarts_ShouldInitializeWithZeroStats()
+        {
+            var game = new QuizGame();
+
+            Assert.Equal(0, game.Score);
+            Assert.Equal(0, game.CorrectAnswers);
+            Assert.Equal(0, game.TotalQuestionsAnswered);
+            Assert.Equal(0.0, game.CorrectAnswerPercentage);
+        }
+
+        [Fact]
+        public void GiveAnswer_WhenCorrect_ShouldIncreaseScoreAndCounts()
+        {
+            var game = new QuizGame();
+            int points = 10;
+
+            game.GiveAnswer(true, points);
+
+            Assert.Equal(10, game.Score);
+            Assert.Equal(1, game.CorrectAnswers);
+            Assert.Equal(1, game.TotalQuestionsAnswered);
+        }
+        [Fact]
+        public void GiveAnswer_WhenIncorrect_ShouldOnlyIncreaseTotalCount()
+        {
+            var game = new QuizGame();
+            int points = 10;
+
+            game.GiveAnswer(false, points);
+
+            Assert.Equal(0, game.Score);
+            Assert.Equal(0, game.CorrectAnswers);
+            Assert.Equal(1, game.TotalQuestionsAnswered);
+        }
+
+        [Fact]
+        public void CorrectAnswerPercentage_When1Of1_ShouldBe100()
+        {
+            var game = new QuizGame();
+
+            game.GiveAnswer(true, 10);
+
+            Assert.Equal(100.0, game.CorrectAnswerPercentage, 2);
+        }
+        [Fact]
+        public void CorrectAnswerPercentage_When1Of2_ShouldBe50()
+        {
+            var game = new QuizGame();
+
+            game.GiveAnswer(true, 10);
+            game.GiveAnswer(false, 10);
+
+            Assert.Equal(50.0, game.CorrectAnswerPercentage, 2);
+        }
+        [Fact]
+        public void CorrectAnswerPercentage_When3Of4_ShouldBe75()
+        {
+            var game = new QuizGame();
+
+            game.GiveAnswer(true, 10);
+            game.GiveAnswer(true, 10);
+            game.GiveAnswer(true, 10);
+            game.GiveAnswer(false, 10);
+
+            Assert.Equal(75.0, game.CorrectAnswerPercentage, 2);
+        }
+        [Fact]
+        public void CorrectAnswerPercentage_WhenNoAnswers_ShouldBeZero()
+        {
+            var game = new QuizGame();
+            Assert.Equal(0.0, game.CorrectAnswerPercentage);
+        }
+        [Fact]
+        public void Restart_WhenCalled_ShouldResetAllStats()
+        {
+            var game = new QuizGame();
+            game.GiveAnswer(true, 10);
+            game.GiveAnswer(false, 15);
+            game.GiveAnswer(true, 5);
+
+            game.Restart();
+
+            Assert.Equal(0, game.Score);
+            Assert.Equal(0, game.CorrectAnswers);
+            Assert.Equal(0, game.TotalQuestionsAnswered);
+            Assert.Equal(0.0, game.CorrectAnswerPercentage);
+        }
+    }    
+}
